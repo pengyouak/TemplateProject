6b0a09e3e9a9a8febf933243c9bbc68fcc021253
付金龙 <fujinlong@bjknrt.com>
2019-04-28 09:41:34 +0800

    添加日志

---
+++ TemplateProject/Log/Logger.cs
@@ -0,0 +1,83 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace TemplateProject.Log
+{
+    public static class Logger
+    {
+        private static string GetCurrentMethodFullName()
+        {
+            StackFrame frame;
+            string str;
+            string str1;
+            bool flag;
+            try
+            {
+                int num = 2;
+                StackTrace stackTrace = new StackTrace();
+                int length = stackTrace.GetFrames().Length;
+                do
+                {
+                    int num1 = num;
+                    num = num1 + 1;
+                    frame = stackTrace.GetFrame(num1);
+                    str = frame.GetMethod().DeclaringType.ToString();
+                    flag = (!str.EndsWith("Exception") ? false : num < length);
+                }
+                while (flag);
+                string name = frame.GetMethod().Name;
+                str1 = string.Concat(str, ".", name);
+            }
+            catch
+            {
+                str1 = null;
+            }
+
+            Console.WriteLine(str1);
+            return str1;
+        }
+
+        public static void Debug(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Debug(message);
+        }
+
+        public static void Debug(object message, Exception ex)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Debug(message, ex);
+        }
+
+        public static void Error(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Error(message);
+        }
+
+        public static void Error(object message, Exception exception)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Error(message, exception);
+        }
+
+        public static void Info(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Info(message);
+        }
+
+        public static void Info(object message, Exception ex)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Info(message, ex);
+        }
+
+        public static void Warn(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Warn(message);
+        }
+
+        public static void Warn(object message, Exception ex)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Warn(message, ex);
+        }
+    }
+}
